a192f46c6ad572587f1411c6eda1297ef07b172d
moonlightleonbots <mullermatthias034@gmail.com>
2025-06-08 16:35:21 +0200

    Update index.js

--- lambda/index.js
+++ lambda/index.js
@@ -4,8 +4,8 @@
  * session persistence, api calls, and more.
  * */
  
-const stationName = "Brovenco Webradio"
-const spokenStationName = "Brovenco Webradio"
+const stationName = "Bochum Radio"
+const spokenStationName = "Bochum Radio"
 
 const Alexa = require('ask-sdk-core');
 const messages = {
